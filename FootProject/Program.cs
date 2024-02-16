a91a8507574304c0dda5b37b4ccd43a76c2ce638
alexandredach <alexandre.dach@ikmail.com>
2024-02-16 13:42:37 +0100

    Lv3 sans héritage

--- FootProject/Program.cs
+++ FootProject/Program.cs
@@ -7,24 +7,24 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            TeamsManager.GiveCard(TeamChauneSurMarne.PlayersOnField, Card.Red, "Tarek", "TIFFIEH");
+            TeamsManager.DisplayTeamByList(Cities.ChauneSurMarne, TeamsManager.PlayersOnField);
             Console.WriteLine();
 
-            //TeamChauneSurMarne.GiveCard(Card.Red,"Tarek","TIFFIEH");
+            TeamsManager.GiveCard(Card.Red, "Tarek", "TIFFIEH");
 
-            //TeamChauneSurMarne.GiveCard(Card.Yellow, "Roger", "TROBU");
-            //TeamChauneSurMarne.GiveCard(Card.Yellow, "Roger", "TROBU");
+            TeamsManager.GiveCard(Card.Yellow, "Roger", "TROBU");
+            TeamsManager.GiveCard(Card.Yellow, "Roger", "TROBU");
 
-            //Console.WriteLine("NOUVELLE EQUIPE :");
-            //TeamChauneSurMarne.DisplayTeamByList(Cities.ChauneSurMarne, TeamChauneSurMarne.PlayersOnField);
+            Console.WriteLine("NOUVELLE EQUIPE :");
+            TeamsManager.DisplayTeamByList(Cities.ChauneSurMarne, TeamsManager.PlayersOnField);
 
-            //Console.WriteLine();
-            //Console.WriteLine("Remplaçants :");
-            //TeamChauneSurMarne.DisplayTeamByList(Cities.ChauneSurMarne, TeamChauneSurMarne.PlayersSubstitute);
+            Console.WriteLine();
+            Console.WriteLine("Remplaçants :");
+            TeamsManager.DisplayTeamByList(Cities.ChauneSurMarne, TeamsManager.PlayersSubstitute);
 
-            //Console.WriteLine();
-            //Console.WriteLine("Exclus :");
-            //TeamChauneSurMarne.DisplayTeamByList(Cities.ChauneSurMarne, TeamChauneSurMarne.PlayersSentOff);
+            Console.WriteLine();
+            Console.WriteLine("Exclus :");
+            TeamsManager.DisplayTeamByList(Cities.ChauneSurMarne, TeamsManager.PlayersSentOff);
 
 
 
--- FootProject/TeamsManager.cs
+++ FootProject/TeamsManager.cs
@@ -1,35 +1,84 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace FootProject
 {
-	internal class TeamsManager
-	{
+    internal class TeamsManager
+    {
 
+        public static List<Player> PlayersOnField = new List<Player>()
+        {
+            new Player(1 , "Alain", "PROVISTE", Cities.ChauneSurMarne, Status.On_Field),
+            new Player(3 , "Tarek", "TIFFIEH", Cities.ChauneSurMarne, Status.On_Field),
+            new Player(4 , "Guy", "LIGUILLI", Cities.ChauneSurMarne, Status.On_Field),
+            new Player(7 , "Hal", "COLIK", Cities.ChauneSurMarne, Status.On_Field),
+            new Player(8 , "Franck", "FORT", Cities.ChauneSurMarne, Status.On_Field),
+            new Player(12 , "Sam", "AGACE", Cities.ChauneSurMarne, Status.On_Field),
+            new Player(13 , "Roger", "TROBU", Cities.ChauneSurMarne, Status.On_Field),
+            new Player(15 , "Gérard", "MANVUSSA", Cities.ChauneSurMarne, Status.On_Field),
+            new Player(17 , "Andy", "SCOTEK", Cities.ChauneSurMarne, Status.On_Field),
+            new Player(18 , "Basile", "HIC", Cities.ChauneSurMarne, Status.On_Field),
+
+            new Player(1 , "Kylian", "MBAPPE", Cities.Paris, Status.On_Field),
+            new Player(3 , "Ousmane", "DEMBELE", Cities.Paris, Status.On_Field),
+            new Player(4 , "Marco", "ASENSIO", Cities.Paris, Status.On_Field),
+            new Player(7 , "Randal", "KOLO MUANI", Cities.Paris, Status.On_Field),
+            new Player(8 , "Bryan", "BARCOLA", Cities.Paris, Status.On_Field),
+            new Player(12 , "Achraf", "HAKIMI", Cities.Paris, Status.On_Field),
+            new Player(13 , "Mauricio", "UGARTE", Cities.Paris, Status.On_Field),
+            new Player(15 , "Lucas", "HERNANDEZ", Cities.Paris, Status.On_Field),
+            new Player(17 , "Thimotée", "PEMBELE", Cities.Paris, Status.On_Field),
+            new Player(18 , "Arnau", "TENAS", Cities.Paris, Status.On_Field)
+        };
+
+        public static List<Player> PlayersSubstitute = new List<Player>()
+        {
+            new Player(2 , "Phil", "DEFER", Cities.ChauneSurMarne, Status.Substitute),
+            new Player(5 , "Gaspard", "ALIZAN", Cities.ChauneSurMarne, Status.Substitute),
+            new Player(6 , "Harry", "COBLAN", Cities.ChauneSurMarne, Status.Substitute),
+            new Player(9 , "Axel", "HERRE", Cities.ChauneSurMarne, Status.Substitute),
+            new Player(10 , "Lee", "BERTINE", Cities.ChauneSurMarne, Status.Substitute),
+
+            new Player(2 , "Keylor", "NAVAS", Cities.Paris, Status.Substitute),
+            new Player(5 , "Sergio", "RICO", Cities.Paris, Status.Substitute),
+            new Player(6 , "Nordi", "MUKIELE", Cities.Paris, Status.Substitute),
+            new Player(9 , "Carlos", "SOLER", Cities.Paris, Status.Substitute),
+            new Player(10 , "Thilo", "KEHRER", Cities.Paris, Status.Substitute)
+        };
+
+        public static List<Player> PlayersSentOff = new List<Player>();
+
+        // LISTE D'ATTENTE A FAIRE POUR MATCH SUIVANT SI BESOIN : les remplaçants entrent de suite dans la liste
 
         public static void DisplayTeamByList(Cities cityName, List<Player> listName)
+        {
+            foreach (Player player in listName)
             {
-                foreach (Player player in listName)
-                {
-                    if (player.CityName.Equals(cityName))
-                        Console.WriteLine($"[{player.Number}] {player.FirstName} {player.LastName} {player.YellowCards} {player.Status} ");
-                }
+                if (player.CityName.Equals(cityName))
+                    Console.WriteLine($"[{player.Number}] ({player.CityName}) {player.FirstName} {player.LastName} {player.YellowCards} {player.Status} ");
             }
+        }
 
-        public static void GiveCard(TeamDataBase teamName, Card cardColor, string playerFirstName, string playerLastName)
+        public static void GiveCard(Card cardColor, string playerFirstName, string playerLastName)
         {
             if (cardColor == Card.Red)
             {
-                foreach (Player player in teamName.PlayersOnField)
+                foreach (Player player in PlayersOnField)
                     if (player.FirstName.Contains(playerFirstName) && player.LastName.Contains(playerLastName))
                     {
                         player.Status = Status.SentOff_Red_Card;
                         Console.WriteLine($"CARTON ROUGE ! {player.FirstName} {player.LastName} a été exclu !");
-                        ReplacePlayer(teamName, player);
+                        ReplacePlayer(player);
                         break;
                     }
             }
             else if (cardColor == Card.Yellow)
             {
-                foreach (Player player in teamName.PlayersOnField)
+                foreach (Player player in PlayersOnField)
                 {
                     if (player.FirstName.Contains(playerFirstName) && player.LastName.Contains(playerLastName))
                     {
@@ -38,7 +87,7 @@ namespace FootProject
                         {
                             player.Status = Status.SentOff_Yellow_Cards;
                             Console.WriteLine($"2 CARTONS JAUNES ! {player.FirstName} {player.LastName} a été exclu !");
-                            ReplacePlayer(teamName, player);
+                            ReplacePlayer(player);
                             break;
                         }
                     }
@@ -46,38 +95,38 @@ namespace FootProject
             }
         }
 
-        public static void PlayerInjured(TeamDataBase teamName, string playerFirstName, string playerLastName)
+        public static void PlayerInjured(string playerFirstName, string playerLastName)
         {
 
-            foreach (Player player in teamName.PlayersOnField)
+            foreach (Player player in PlayersOnField)
                 if (player.FirstName.Contains(playerFirstName) && player.LastName.Contains(playerLastName))
                 {
                     player.Status = Status.Inured;
                     Console.WriteLine($"{player.FirstName} {player.LastName} s'est blessé et est sorti du terrain.");
-                    ReplacePlayer(teamName, player);
+                    ReplacePlayer(player);
                     break;
                 }
 
         }
-        
-        public static void ReplacePlayer(TeamDataBase teamName, Player playerToReplace)
+
+        public static void ReplacePlayer(Player playerToReplace)
         {
-            teamName.PlayersOnField.Remove(playerToReplace);
-            teamName.PlayersSentOff.Add(playerToReplace);
+            PlayersOnField.Remove(playerToReplace);
+
+            PlayersSentOff.Add(playerToReplace);
             // je prends l'index d'un joueur de la liste des remplaçants aléatoirement ...
-            int randomPlayerSubstitute = new Random().Next(0, teamName.PlayersSubstitute.Count);
+
+            int randomPlayerSubstitute = new Random().Next(0, PlayersSubstitute.Count);
             // ... pour l'ajouter à la liste des joueurs sur le terrain ...
-            teamName.PlayersOnField.Add(teamName.PlayersSubstitute[randomPlayerSubstitute]);
-            teamName.PlayersSubstitute[randomPlayerSubstitute].Status = Status.On_Field;
+            PlayersOnField.Add(PlayersSubstitute[randomPlayerSubstitute]);
+            PlayersSubstitute[randomPlayerSubstitute].Status = Status.On_Field;
             // CONSOLE
-            Console.WriteLine($"{teamName.PlayersSubstitute[randomPlayerSubstitute].FirstName} {teamName.PlayersSubstitute[randomPlayerSubstitute].LastName} est entré sur le terrain.");
+            Console.WriteLine($"{PlayersSubstitute[randomPlayerSubstitute].FirstName} {PlayersSubstitute[randomPlayerSubstitute].LastName} est entré sur le terrain.");
             // ... et je le retire de la liste des remplaçants
             // CONSOLE
-            Console.WriteLine($"{teamName.PlayersSubstitute[randomPlayerSubstitute].FirstName} {teamName.PlayersSubstitute[randomPlayerSubstitute].LastName} : sorti de la liste des remplaçants\n");
-            teamName.PlayersSubstitute.RemoveAt(randomPlayerSubstitute);
+            Console.WriteLine($"{PlayersSubstitute[randomPlayerSubstitute].FirstName} {PlayersSubstitute[randomPlayerSubstitute].LastName} : sorti de la liste des remplaçants\n");
+            PlayersSubstitute.RemoveAt(randomPlayerSubstitute);
 
         }
-        
-	}
+    }
 }
-
